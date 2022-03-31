2705ebd0ae5762b51c5b7b07dbea92ed825f669e
Andy <andy8647lgc@gmail.com>
2022-03-31 12:42:10 -0400

    use translate for better performance

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,7 +7,10 @@
   >
     <div
       class="list-item"
-      :style="{ backgroundColor: bgColors[index], zIndex: index }"
+      :style="[
+        { backgroundColor: bgColors[index], zIndex: index, minHeight: listItemHeight },
+        itemTranslateCSS(index),
+      ]"
       :ref="
         (div) => {
           if (index === 0) {
@@ -24,15 +27,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, ComputedRef } from 'vue';
+import { ref, computed, watch, ComputedRef } from 'vue';
 import { generateRandomColors } from '@/utils/randomColors';
 
-let itemMargin = ref<Number>(20);
-const itemMarginCSS: ComputedRef = computed(() => {
-  return `-${itemMargin.value}px`;
-});
+const initTranslate: number = 0;
+const itemTranslate = ref<Number>(initTranslate);
+const itemTranslateCSS = (index: number): string => {
+  if (isTouching.value && Math.abs(Number(distanceSwiped.value)) > 0) {
+    // calculate the offset value depend on the index of the element
+    const offSetY =
+      Number(itemTranslate.value) + (index * Number(listItemHeight.value.slice(0, 3))) / 2;
+    return `transform: translate3D(0, ${offSetY}px, 0)`;
+  }
+  return `transform: translate3D(0, ${itemTranslate.value}px, 0)`;
+};
 
 const listItemNum = ref<Number>(20);
+const listItemHeight = ref<String>('100px');
 const firstListItem = ref<HTMLElement>(document.createElement('div'));
 const lastListItem = ref<HTMLElement>(document.createElement('div'));
 
@@ -48,33 +59,45 @@ const getFirstAndLastItemPosition = (): void => {
   isFirstItemScrollToTop.value = firstItemPosition >= 0;
   isLastItemScrollToBottom.value =
     lastItemPosition + lastListItem.value.offsetHeight <= viewPortHeight;
-  console.log(firstItemPosition, isFirstItemScrollToTop);
-};
-
-const resizeMargin = (): void => {
-  const distanceSwiped = Number(touchingY.value) - Number(touchStartY.value);
-  if (distanceSwiped > 0) {
-    itemMargin.value = distanceSwiped;
-  }
 };
 
 let touchStartY = ref<Number>(0);
 let touchingY = ref<Number>(0);
 let touchEndY = ref<Number>(0);
+let distanceSwiped = ref<Number>(0);
+
+watch(distanceSwiped, (newDistance, oldDistance) => {
+  itemTranslate.value = newDistance;
+});
+
+const isTouching = ref<Boolean>(false);
+const enlargeTranslate = (): void => {
+  itemTranslate.value = distanceSwiped.value;
+};
+
 const onTouchStart = (event: TouchEvent): void => {
+  isTouching.value = true;
+  // get the start position of touch event
   touchStartY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
 };
 const onTouchMove = (event: TouchEvent): void => {
+  isTouching.value = true;
+  // update the touching position during the event
   touchingY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
-  if (isFirstItemScrollToTop.value) {
-    resizeMargin();
+  // if the first element is already scrolled to top and keep swiping down, enlarge the translate
+  if (isFirstItemScrollToTop.value || isLastItemScrollToBottom.value) {
+    updateSwipeDistance(event);
+    enlargeTranslate();
   }
 };
 const onTouchEnd = (event: TouchEvent): void => {
+  isTouching.value = false;
   touchEndY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
+  // reset the translate to init
+  itemTranslate.value = initTranslate;
 };
 
 const getTouchPosition = (event: TouchEvent): Number => {
@@ -85,6 +108,17 @@ const getTouchPosition = (event: TouchEvent): Number => {
   }
 };
 
+const updateSwipeDistance = (event: TouchEvent): void => {
+  switch (event.type) {
+    case 'touchmove':
+      distanceSwiped.value = Number(touchingY.value) - Number(touchStartY.value);
+      break;
+    case 'touchend':
+      distanceSwiped.value = Number(touchEndY.value) - Number(touchStartY.value);
+      break;
+  }
+};
+
 const bgColors = ref(generateRandomColors(Number(listItemNum.value)));
 </script>
 
@@ -92,8 +126,8 @@ const bgColors = ref(generateRandomColors(Number(listItemNum.value)));
 .list-item {
   width: 100%;
   min-height: 10vh;
-  margin-bottom: v-bind(itemMarginCSS);
   box-shadow: -20px -10px 19px 13px rgb(0 0 0 / 10%);
   border-radius: 8px;
+  margin-bottom: -20px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/swipe-list/",
+  devServer: {
+    open: "Google Chrome",
+  },
 });
