f2bb2fbc0879f295f8abef3ca5fa1521c018c119
Andy <andy8647lgc@gmail.com>
2022-03-30 22:49:24 -0400

    add margin resize when scroll to top

--- src/components/List.vue
+++ src/components/List.vue
@@ -24,12 +24,12 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, computed, ComputedRef } from 'vue';
+import { ref, computed, ComputedRef } from 'vue';
 import { generateRandomColors } from '@/utils/randomColors';
 
-const initMargin = reactive<Number>(20);
-const initMarginCSS: ComputedRef = computed(() => {
-  return `-${initMargin}px`;
+let itemMargin = ref<Number>(20);
+const itemMarginCSS: ComputedRef = computed(() => {
+  return `-${itemMargin.value}px`;
 });
 
 const listItemNum = ref<Number>(20);
@@ -39,29 +39,41 @@ const lastListItem = ref<HTMLElement>(document.createElement('div'));
 const viewPortHeight =
   window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-let touchStartY = reactive<Number>(0);
-let touchingY = reactive<Number>(0);
-let touchEndY = reactive<Number>(0);
-let isFirstItemScrollToTop = reactive<Boolean>(true);
-let isLastItemScrollToBottom = reactive<Boolean>(false);
+let isFirstItemScrollToTop = ref<Boolean>(true);
+let isLastItemScrollToBottom = ref<Boolean>(false);
 
 const getFirstAndLastItemPosition = (): void => {
   const firstItemPosition: number = firstListItem.value.getBoundingClientRect().y;
   const lastItemPosition: number = Math.floor(lastListItem.value.getBoundingClientRect().y);
-  isFirstItemScrollToTop = firstItemPosition > 0;
-  isLastItemScrollToBottom = lastItemPosition + lastListItem.value.offsetHeight <= viewPortHeight;
+  isFirstItemScrollToTop.value = firstItemPosition >= 0;
+  isLastItemScrollToBottom.value =
+    lastItemPosition + lastListItem.value.offsetHeight <= viewPortHeight;
+  console.log(firstItemPosition, isFirstItemScrollToTop);
 };
 
+const resizeMargin = (): void => {
+  const distanceSwiped = Number(touchingY.value) - Number(touchStartY.value);
+  if (distanceSwiped > 0) {
+    itemMargin.value = distanceSwiped;
+  }
+};
+
+let touchStartY = ref<Number>(0);
+let touchingY = ref<Number>(0);
+let touchEndY = ref<Number>(0);
 const onTouchStart = (event: TouchEvent): void => {
-  touchStartY = getTouchPosition(event);
+  touchStartY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
 };
 const onTouchMove = (event: TouchEvent): void => {
-  touchingY = getTouchPosition(event);
+  touchingY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
+  if (isFirstItemScrollToTop.value) {
+    resizeMargin();
+  }
 };
 const onTouchEnd = (event: TouchEvent): void => {
-  touchEndY = getTouchPosition(event);
+  touchEndY.value = getTouchPosition(event);
   getFirstAndLastItemPosition();
 };
 
@@ -80,7 +92,7 @@ const bgColors = ref(generateRandomColors(Number(listItemNum.value)));
 .list-item {
   width: 100%;
   min-height: 10vh;
-  margin-bottom: v-bind(initMarginCSS);
+  margin-bottom: v-bind(itemMarginCSS);
   box-shadow: -20px -10px 19px 13px rgb(0 0 0 / 10%);
   border-radius: 8px;
 }
